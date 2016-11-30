a79faca4af25b524cfc3c684abb18e2c4f08232b
Dom Williams <me@domwillia.ms>
2016-11-30 13:29:01 +0100

    Improve server management script

--- bin/server.sh
+++ bin/server.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-CODEBASE="../classes/"
-SERVER_CLASS="hello.server.Server"
+do_thing() {
+	echo "$@ ... "
+}
+
+
+CODEBASE="$1"
+SERVER_CLASS="chatroom.server.ChatServer"
 
 cd $CODEBASE || exit 1
+
+do_thing Killing existing rmiregistry
 pkill rmiregistry
+
+do_thing Starting new rmiregistry
 rmiregistry &
+
+do_thing Waiting for rmiregistry to startup
 sleep 0.5
+
+do_thing Starting server
 java -cp . -Djava.rmi.server.codebase=file:./ $SERVER_CLASS
+
+do_thing Shutting down
